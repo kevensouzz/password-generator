9385976e6b3cb455064849d644581f614901f04f
Keven <kevenssouza1@gmail.com>
2023-01-10 11:53:42 -0300

    Update files

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+const botaoGerar = document.querySelector("#btngerar")
 const botaoCopiar = document.querySelector("#btncopiar")
 const password = document.querySelector("#password")
+const inputRange = document.querySelector("#lenght")
+
+inputRange.addEventListener("input", function inputValueNow() {
+    var inputRangeValueNow = this.value
+})
 
 botaoCopiar.addEventListener("click", () => {
     navigator.clipboard.writeText(password.innerText)
